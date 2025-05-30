6fc55132e4b1ae26857b9f53bc1778ec31d081af
champs-8 <fabio.s.dev@outlook.com>
2025-05-30 09:15:43 -0300

    Adiciona ícone ao cabeçalho, atualiza a versão e ajusta estilos de layout

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
-const API_URL = 'https://jogodavelha-xks4.onrender.com';
-// const API_URL = 'http://localhost:8080'; //url de teste
+// const API_URL = 'https://jogodavelha-xks4.onrender.com';
+const API_URL = 'http://localhost:8080'; //url de teste
 
 const tabuleiro = document.getElementById('tabuleiro');
 
